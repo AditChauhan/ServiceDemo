e470d946159192f82503f0c009a6a8577403eede
i-have-a-fix <aditchauhan.ac@gmail.com>
2019-03-16 12:58:27 +0530

    Boundservice updated manifest declaration

--- app/src/main/java/tutorial/adit/com/servicedemo/MainActivity.java
+++ app/src/main/java/tutorial/adit/com/servicedemo/MainActivity.java
@@ -1,8 +1,13 @@
 package tutorial.adit.com.servicedemo;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,16 +26,21 @@ public class MainActivity extends AppCompatActivity {
         printTimestampButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("onclick status",""+mServiceBound);
                 if (mServiceBound) {
                     timestampText.setText(mBoundService.getTimestamp());
                 }
             }
         });
 
-        stopServiceButon.setOnClickListener(new View.OnClickListener() {
+        
+
+        stopServiceButon.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v) {
                 if (mServiceBound) {
+                    unbindService(mServiceConnection);
                     mServiceBound = false;
                 }
                 Intent intent = new Intent(MainActivity.this,
@@ -46,16 +56,31 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         Intent intent = new Intent(this, BoundService.class);
         startService(intent);
+        bindService(intent, mServiceConnection, Context.BIND_AUTO_CREATE);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
         if (mServiceBound) {
+            unbindService(mServiceConnection);
             mServiceBound = false;
         }
     }
 
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
 
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mServiceBound = false;
+        }
 
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d("service","connected");
+            BoundService.MyBinder myBinder = (BoundService.MyBinder) service;
+            mBoundService = myBinder.getService();
+            mServiceBound = true;
+        }
+    };
 }

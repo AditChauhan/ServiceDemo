77a9eab2353d6b8f8882f5a7f8168d44919432ca
i-have-a-fix <aditchauhan.ac@gmail.com>
2019-03-16 10:36:22 +0530

    Initial commit

---
+++ app/src/main/java/tutorial/adit/com/servicedemo/MainActivity.java
@@ -0,0 +1,61 @@
+package tutorial.adit.com.servicedemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    BoundService mBoundService;
+    boolean mServiceBound = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextView timestampText = (TextView) findViewById(R.id.timestamp_text);
+        Button printTimestampButton = (Button) findViewById(R.id.print_timestamp);
+        Button stopServiceButon = (Button) findViewById(R.id.stop_service);
+        printTimestampButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mServiceBound) {
+                    timestampText.setText(mBoundService.getTimestamp());
+                }
+            }
+        });
+
+        stopServiceButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mServiceBound) {
+                    mServiceBound = false;
+                }
+                Intent intent = new Intent(MainActivity.this,
+                        BoundService.class);
+                stopService(intent);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, BoundService.class);
+        startService(intent);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mServiceBound) {
+            mServiceBound = false;
+        }
+    }
+
+
+
+}
